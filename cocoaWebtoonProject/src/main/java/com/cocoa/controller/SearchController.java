11350f65b6bbeaada1b01523189bb5a66ca419e9
choihyeji <hyejichoi14@gmail.com>
2025-01-19 00:44:34 +0900

    SearchController의 /search 코드 변경

--- cocoaWebtoonProject/src/main/java/com/cocoa/controller/SearchController.java
+++ cocoaWebtoonProject/src/main/java/com/cocoa/controller/SearchController.java
@@ -1,38 +1,32 @@
 package com.cocoa.controller;
 
 import java.util.List;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-
 import com.cocoa.domain.WebToonDTO;
 import com.cocoa.service.WebToonService;
-
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j;
 
 @Controller
 @Log4j
-@RequestMapping("/*")
+@RequestMapping("/search")
 @RequiredArgsConstructor
 public class SearchController {
 	
 	private final WebToonService webtoonservice;
 	
-	@GetMapping("/search")
-	public void search(@RequestParam(name="keyword", required = false) String searchbox, Model model){
-		log.info("keyword : " + searchbox);
-		if(searchbox != null) {
-			if(searchbox != "") {
-			List<WebToonDTO> webtoons = webtoonservice.search(searchbox);
+	@GetMapping
+	public void search(@RequestParam(name="keyword", required = false) String keyword, Model model){
+		log.info("Search keyword: " + keyword);
+		
+		if(keyword != null && !keyword.trim().isEmpty()) {		
+			List<WebToonDTO> webtoons = webtoonservice.search(keyword);
 			model.addAttribute("webtoons", webtoons);
-			}
-		}	
-	
+		}			
 	}
 
 }
